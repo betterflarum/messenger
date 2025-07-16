8382ffaebbf7f0b221be85de9a2d14b5a4b86e0d
BetterFlarum <143252203+betterflarum@users.noreply.github.com>
2025-07-17 01:57:49 +0700

    Add files via upload

---
+++ extend.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BetterFlarum\NextAdminUI;
+
+use Flarum\Extend;
+
+return [
+        (new Extend\Frontend('forum'))
+        ->css(__DIR__.'/less/forum.less'),
+    (new Extend\Frontend('admin'))
+        ->css(__DIR__.'/less/admin.less')
+];
